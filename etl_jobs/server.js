88c2a346ac920fa80b18941b8fb6f864163605e8
Mathieu Rhéaume <mathieu@codingrhemes.com>
2014-12-28 21:35:12 -0500

    Added reccurence rules !

--- etl_jobs/server.js
+++ etl_jobs/server.js
@@ -4,11 +4,23 @@
 // =============================================================================
 
 // call the packages we need
+var schedule = require('node-schedule');
 var express    = require('express'); 		// call express
 var app        = express(); 				// define our app using express
 var bodyParser = require('body-parser');
 var VQ_CONSTS = require('./app/VQ_CONSTS.js'); // VQ Related specific logic
 
+
+function doRssLogic() {
+    if (VQ_CONSTS.ShouldFetchFromAPI() === true) {
+    VQ_CONSTS.FetchDataFromAPI();
+    } else {
+        console.log('Skipping exec ! ');
+    }
+}
+
+var rule = new schedule.RecurrenceRule();
+rule.minute = [15, 30, 45];
 
 // configure app to use bodyParser()
 // this will let us get the data from a POST
@@ -18,8 +30,7 @@ app.use(bodyParser.json());
 console.log('Magic happens ');
 
 
-if (VQ_CONSTS.ShouldFetchFromAPI() === true) {
-VQ_CONSTS.FetchDataFromAPI();
-} else {
-    console.log('Skipping exec ! ');
-}
+
+var j = schedule.scheduleJob(rule, function(){
+    doRssLogic();
+});
