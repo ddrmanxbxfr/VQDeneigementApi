e83674e2cce38537692d36a3759bd3fceea65061
Mathieu Rhéaume <mathieu@codingrhemes.com>
2014-12-28 22:17:20 -0500

    Do first data load on startup

--- etl_jobs/server.js
+++ etl_jobs/server.js
@@ -28,7 +28,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 console.log('Magic happens ');
-
+doRssLogic();
 
 
 var j = schedule.scheduleJob(rule, function(){
